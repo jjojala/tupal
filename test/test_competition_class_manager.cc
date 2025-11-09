b6fada61f9a34b7e08240b70f6367f9e2fb542fb
Jari Ojala <jari.ojala@iki.fi>
2025-11-09 16:21:05 +0200

    test and implement competition_class_manager::create()

--- test/test_competition_class_manager.cc
+++ test/test_competition_class_manager.cc
@@ -131,3 +131,54 @@ TEST_CASE("competition class manager get (existing)") {
     CHECK(fetched_competition_class.is_object());
     CHECK(fetched_competition_class.as_object().at("id").as_string() == "class-001");
 }
+
+TEST_CASE("competition class manager create (comp not found)") {
+    auto competition_manager = tupal::CompetitionManager::new_competition_manager("sqlite3://:memory:");
+    auto competition_class_manager = competition_manager->getCompetitionClassManager();
+
+    boost::json::value new_competition_class = boost::json::object {
+        { "id", "class-001" },
+        { "title", "P10" },
+        { "start_group_id", "sg-001" }
+    };
+
+    auto [create_ec, created_competition_class] = competition_class_manager->create("comp-001", new_competition_class);
+    CHECK(create_ec == tupal::make_error_condition(tupal::error_code::unknown_key));
+}
+
+TEST_CASE("competition class manager create and duplicate") {
+    auto competition_manager = tupal::CompetitionManager::new_competition_manager("sqlite3://:memory:");
+    auto start_group_manager = competition_manager->getStartGroupManager();
+    auto competition_class_manager = competition_manager->getCompetitionClassManager();
+
+    auto [comp_create_ec, created_competition] = competition_manager->create(
+        boost::json::object {
+            { "id", "comp-001" },
+            { "date", "2024-01-01" },
+            { "title", "New Year Competition" }
+        });
+    CHECK(!comp_create_ec);
+
+    auto [sg_create_ec, created_start_group] = start_group_manager->create("comp-001",
+        boost::json::object {
+            { "id", "sg-001" },
+            { "title", "Start Group 1" },
+            { "start_time", "2024-01-01T10:00:00Z" },
+            { "first_bib", 100 }
+        });
+    CHECK(!sg_create_ec);
+
+    boost::json::value new_competition_class = boost::json::object {
+        { "id", "class-001" },
+        { "title", "P10" },
+        { "start_group_id", "sg-001" }
+    };
+
+    auto [create_ec, created_competition_class] = competition_class_manager->create("comp-001", new_competition_class);
+    CHECK(!create_ec);
+    CHECK(created_competition_class.is_object());
+    CHECK(created_competition_class.as_object().at("id").as_string() == "class-001");
+
+    auto [create_ec2, created_competition2] = competition_class_manager->create("comp-001", new_competition_class);
+    CHECK(create_ec2 == tupal::make_error_condition(tupal::error_code::duplicate_key));
+}
