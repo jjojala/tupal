8636aeba64ff8311be8a56fd9f09985baa854e3b
Jari Ojala <jari.ojala@iki.fi>
2026-03-20 23:22:49 +0200

    Test case for "create, read, update, and delete a competition"

--- test/rest_test.cc
+++ test/rest_test.cc
@@ -9,9 +9,12 @@
 namespace {
     const std::string base_url = "http://localhost:8085";
 
-    /** @see definition below */
+    /**{ declarations, @see definitions below */
+    bool contains(const boost::json::value &, const boost::json::array &);
     bool contains(const boost::json::value &, const boost::json::value &);
+    /**} */
 
+    /** @brief returns true, if all fields of `obj` are contained by `item`. */
     bool contains(const boost::json::object & obj, const boost::json::value & item) {
         if (item.is_object()) {
             bool status = true;
@@ -33,14 +36,28 @@ namespace {
         return false;
     }
 
+    /** @brief returns true, if `val` is included in array `items`. */
+    bool contains(const boost::json::value & val, const boost::json::array & items) {
+        for (const auto item: items)
+            if (contains(val, item))
+                return true;
+        return false;
+    }
+
+    /** @brief returns true, if `val` is included in `item` */
     bool contains(const boost::json::value & val, const boost::json::value & item) {
         if (val.is_object())
             return contains(val.as_object(), item);
 
         else if (val.is_array()) {
+            if (!item.is_array()) {
+                MESSAGE("Item '", item, "' expected to be an array, but it is not");
+                return false;
+            }
+
             bool status = true;
-            for (const auto values: val.as_array())
-                if (!contains(values, item))
+            for (const auto v: val.as_array())
+                if (!contains(v, item.as_array()))
                     status = false;
             return status;
         }
@@ -100,10 +117,87 @@ namespace {
     };
 }
 
-#define CHECK_RESPONSE_CREATED(resp) { \
-    auto [ec, response] = resp; \
+boost::json::value to_json(const char * str) { return boost::json::parse(str); }
+boost::json::value to_json(const std::string & str) { return boost::json::parse(str); }
+
+std::string to_list(const char * str) { return boost::json::serialize(boost::json::array { to_json(str) }); }
+
+std::string update_json(const char * str, void (*update)(boost::json::object & val)) {
+    auto json = to_json(str).as_object();
+    update(json);
+    return boost::json::serialize(json);
+}
+
+#define TUPAL_EXPECT_NOTIFICATION(messages, initialCount) { \
+    while (messages.size() <= initialCount) { \
+        std::this_thread::sleep_for(std::chrono::milliseconds(50)); \
+    } \
+}
+
+#define TUPAL_CHECK_STATUS(f, expectedStatus) { \
+    auto [ec, response] = f(); \
     CHECK(ec == std::error_code {}); \
-    CHECK(response.status() == boost::beast::http::status::created); }
+    CHECK(response.status() == expectedStatus); \
+}
+
+#define TUPAL_TEST_GET(listF, expected) { \
+    auto [ec, response] = listF(); \
+    CHECK(ec == std::error_code {}); \
+    CHECK(response.status() == boost::beast::http::status::ok); \
+    MESSAGE("\nExpeced: ", expected, "\nActual : ", to_json(response.body())); \
+    CHECK(contains(to_json(expected), to_json(response.body()))); \
+}
+
+#define TUPAL_TEST_CREATE_NO_NOTIFICATION(createF, data) { \
+    auto [ec, response] = createF(); \
+    CHECK(ec == std::error_code {}); \
+    CHECK(response.status() == boost::beast::http::status::created); \
+    CHECK(contains(to_json(data), to_json(response.body()))); \
+}
+
+#define TUPAL_TEST_CREATE_WITH_NOTIFICATIONS(createF, type, data, messages) { \
+    const auto msgCount = messages.size(); \
+    TUPAL_TEST_CREATE_NO_NOTIFICATION(createF, data); \
+    TUPAL_EXPECT_NOTIFICATION(messages, msgCount); \
+    const auto expected_notification_body { std::string { R"({"op": "created", "type": ")" } + type + R"("})" }; \
+    CHECK(contains(to_json(expected_notification_body), to_json(messages.back()))); \
+    CHECK(contains(to_json(data), to_json(messages.back()).as_object()["item"])); \
+}
+
+#define TUPAL_TEST_UPDATE_NO_NOTIFICATION(updateF, data) { \
+    auto [ec, response] = updateF(); \
+    CHECK(ec == std::error_code {}); \
+    CHECK(response.status() == boost::beast::http::status::ok); \
+    CHECK(contains(to_json(data), to_json(response.body()))); \
+}
+
+#define TUPAL_TEST_UPDATE_WITH_NOTIFICATION(updateF, type, data, messages) { \
+    const auto msgCount = messages.size(); \
+    TUPAL_TEST_UPDATE_NO_NOTIFICATION(updateF, data); \
+    TUPAL_EXPECT_NOTIFICATION(messages, msgCount); \
+    const auto expected_notification_body { std::string { R"({"op": "updated", "type": ")" } + type + R"("})" }; \
+    CHECK(contains(to_json(expected_notification_body), to_json(messages.back()))); \
+    CHECK(contains(to_json(data), to_json(messages.back()).as_object()["item"])); \
+}
+
+#define TUPAL_TEST_REMOVE_NO_NOTIFICATION(deleteF, dataStr) { \
+    auto [ec, response] = deleteF(); \
+    CHECK(ec == std::error_code {}); \
+    CHECK(response.status() == boost::beast::http::status::ok); \
+}
+
+#define TUPAL_TEST_REMOVE_WITH_NOTIFICATION(deleteF, typeStr, dataStr, messages) { \
+    const auto msgCount = messages.size(); \
+    TUPAL_TEST_REMOVE_NO_NOTIFICATION(deleteF, dataStr); \
+    TUPAL_EXPECT_NOTIFICATION(messages, msgCount); \
+    CHECK(contains(to_json(update_json(R"({ "op": "removed" })", [](boost::json::object & val) { \
+        val["type"] = typeStr; \
+        val["item"] = boost::json::object { { "id", dataStr } }; \
+    })), to_json(messages.back()))); \
+}
+
+#define EXPECT_NOTIFICATION(messages) \
+    while (messages.empty()) std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
 TEST_CASE_FIXTURE(daemon_fixture, "WS connect response") {
     const auto comp_data = R"({ "id": "comp-2", "title": "Competition 1", "date": "2025-11-11T00:00:00.000Z" })";
@@ -116,10 +210,10 @@ TEST_CASE_FIXTURE(daemon_fixture, "WS connect response") {
     const auto competitor_data { R"({ "id": "c-1", "comp_class_id": "cc-1", "bib": 101,
             "start_time_offset": "PT600.000S", "finish_time": "2024-01-01T10:59:11.231Z", "status": 0, "name": "Alice" })" };
 
-    CHECK_RESPONSE_CREATED(client.post(base_url + "/rest/competition/", comp_data));
-    CHECK_RESPONSE_CREATED(client.post(comp_url + "/start_group/", start_group_data));
-    CHECK_RESPONSE_CREATED(client.post(comp_url + "/competition_class/", comp_class_data));
-    CHECK_RESPONSE_CREATED(client.post(comp_url + "/competitor/", competitor_data));
+    TUPAL_TEST_CREATE_NO_NOTIFICATION([&]() { return client.post(base_url + "/rest/competition/", comp_data); }, comp_data);
+    TUPAL_TEST_CREATE_NO_NOTIFICATION([&]() { return client.post(comp_url + "/start_group/", start_group_data); }, start_group_data);
+    TUPAL_TEST_CREATE_NO_NOTIFICATION([&]() { return client.post(comp_url + "/competition_class/", comp_class_data); }, comp_class_data);
+    TUPAL_TEST_CREATE_NO_NOTIFICATION([&]() { return client.post(comp_url + "/competitor/", competitor_data); }, competitor_data);
 
     ws_messages_type ws_messages;
     init_ws(comp_id, ws_messages);
@@ -133,15 +227,58 @@ TEST_CASE_FIXTURE(daemon_fixture, "WS connect response") {
     CHECK(contains(boost::json::parse(comp_class_data), msg.at("classes").as_array().front()));
     CHECK(contains(boost::json::parse(competitor_data), msg.at("competitors").as_array().front()));
 }
+
+TEST_CASE_FIXTURE(ws_daemon_fixture, "Create, read, update and delete a competition") {
+    TUPAL_TEST_GET([&]() { return client.get(base_url + "/rest/competition/"); }, "[]");
+
+#if 0 // #44
+    // Get non-existing competition (--> not_found)
+    TUPAL_CHECK_STATUS([&]() { return client.get(base_url + "/rest/competition/comp-1"); }, boost::beast::http::status::not_found);
+#endif
+
+    const auto data = R"({ "id": "comp-1", "title": "Competition 1", "date": "2025-11-11T00:00:00.000Z" })";
+    TUPAL_TEST_CREATE_NO_NOTIFICATION([&]() { return client.post(base_url + "/rest/competition/", data); }, data);
+    TUPAL_TEST_GET([&]() { return client.get(base_url + "/rest/competition/"); }, to_list(data));
+    TUPAL_TEST_GET([&]() { return client.get(base_url + "/rest/competition/comp-1"); }, std::string { data });
+
+    const auto updated_data = update_json(data, [](boost::json::object & val) { val["title"] = "New competition 1"; });
+    TUPAL_TEST_UPDATE_WITH_NOTIFICATION([&]() { 
+        return client.put(base_url + "/rest/competition/comp-1", updated_data.c_str()); },
+        "competition", updated_data.c_str(), ws_messages);
+
+#if 0 // #45  
+// responds with "ok" in case the url contains non-existent id, but the body of the mssage
+// refers to existing id (this is basically "bad data"). However, if message body refers also to non-existent id,
+// one get not_found as expected.
+    TUPAL_CHECK_STATUS([&]() { return client.put(base_url + "/rest/competition/comp-not-fond", updated_data.c_str()); },
+            boost::beast::http::status::not_found);
+#endif
+
+    // Updating non-existent competition (--> not_found)
+    TUPAL_CHECK_STATUS([&]() {
+            return client.put(base_url + "/rest/competition/comp-not-found",
+                R"({
+                    "id": "comp-not-found",
+                    "title": "Competition 1",
+                    "date": "2025-11-11T00:00:00.000Z"
+                })"); }, boost::beast::http::status::not_found);
+
+    // Removing existing competition
+    TUPAL_TEST_REMOVE_WITH_NOTIFICATION([&]() { return client.del(base_url + "/rest/competition/comp-1"); },
+            "competition", "comp-1", ws_messages);
+
+    // Removing non-existent competition (--> not_found)
+    TUPAL_CHECK_STATUS([&]() { return client.del(base_url + "/rest/competition/comp-non-existent"); },
+                boost::beast::http::status::not_found);
 }
 
 TEST_CASE_FIXTURE(ws_daemon_fixture, "rest tests (smoke)") {
 
-    MESSAGE("WebSocket client set up, starting REST tests...");
+#if 0
     CHECK(ws_messages.size() == 1);
     CHECK(ws_messages.front() == R"({})");
     ws_messages.clear();
-
+#endif
     {
         auto [ec, response] = client.get(base_url + "/rest/competition/");
         CHECK(ec == std::error_code {});
